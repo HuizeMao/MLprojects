41a5164630a1a158e3e5ac4c705593db061eac73
apple <apple@appledeMacBook-Pro.local>
2018-09-16 18:25:03 +0200

    init theta

--- HandWrite_Recognition/Learn.m
+++ HandWrite_Recognition/Learn.m
@@ -6,5 +6,7 @@ n = size(X,2);
 
 NetworkLayers = 3;
 Input_Neurons = 784;
-Hiddden_Neurons = 20;
+Hiddden_Neurons = 15;
 Output_Neurons = 10;
+
+Init_Theta = Initialize_Theta(Input_Neurons,Hiddden_Neurons,Output_Neurons);
