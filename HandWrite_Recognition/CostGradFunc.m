72316c60516aabea0daa69d9a9d7dff7acd3c13e
apple <apple@appledeMacBook-Pro.local>
2018-09-16 18:50:58 +0200

    created costGrad

--- HandWrite_Recognition/CostGradFunc.m
+++ HandWrite_Recognition/CostGradFunc.m
@@ -1,3 +1,10 @@
-function [cost, grad] = CostGradFunc(x)
-  
+function [cost, grad] = CostGradFunc(X,Y,Init_Theta,Input_Neurons,Hiddden_Neurons,Output_Neurons)
+  theta1 = reshape(Init_Theta(1:11760),Hiddden_Neurons,Input_Neurons);
+  theta2 =  reshape(Init_Theta(11761:end),Hiddden_Neurons,Output_Neurons);
+  a1 = [1;X];
+  z2 = theta1 * a1;
+  a2 = sigmoid(z2);
+  a2 = [1;a2];
+  z3 = theta2 * a2;
+  a3 = sigmoid(z3);
 end
--- HandWrite_Recognition/Learn.m
+++ HandWrite_Recognition/Learn.m
@@ -1,5 +1,5 @@
 load("mnist.mat");
-X = trainX;
+X = trainX; % X size: 60,000 *784
 y = trainY;
 m = size(X,1);
 n = size(X,2);
@@ -8,5 +8,7 @@ NetworkLayers = 3;
 Input_Neurons = 784;
 Hiddden_Neurons = 15;
 Output_Neurons = 10;
-
+%Theta1 size: 15 * 784
+%Theta2 size: 10 * 15
 Init_Theta = Initialize_Theta(Input_Neurons,Hiddden_Neurons,Output_Neurons);
+%
