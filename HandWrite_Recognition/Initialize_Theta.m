646a8e535308b03c584888c42f7002fe10bc5599
huize mao <2041125835@qq.com>
2018-09-17 14:12:57 +0200

    edit theta

--- HandWrite_Recognition/Initialize_Theta.m
+++ HandWrite_Recognition/Initialize_Theta.m
@@ -1,6 +1,6 @@
 function Theta = Initialize_Theta(nl, nl2, nl3)
-  theta1 = rand(nl2,nl); %size: 15*784
-  theta2 = rand(nl3,nl2); %size: 10 * 15
+  theta1 = rand(nl2,(nl+1)); %size: 15*784
+  theta2 = rand(nl3,(nl2+1)); %size: 10 * 15
   epsilon = 1.5;
   theta1 = theta1 * (2 * epsilon) - epsilon;
   theta2 = theta2 * (2 * epsilon) - epsilon;
--- HandWrite_Recognition/Learn.m
+++ HandWrite_Recognition/Learn.m
@@ -8,7 +8,7 @@ NetworkLayers = 3;
 Input_Neurons = 784;
 Hiddden_Neurons = 15;
 Output_Neurons = 10;
-%Theta1 size: 15 * 784
-%Theta2 size: 10 * 15
+%Theta1 size: 15 * 784+1
+%Theta2 size: 10 * 15+1
 Init_Theta = Initialize_Theta(Input_Neurons,Hiddden_Neurons,Output_Neurons);
 %
