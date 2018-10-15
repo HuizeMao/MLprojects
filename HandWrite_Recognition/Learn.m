213e110bdb71ceeed5393368169f4e321cc29dda
apple <apple@appledeMacBook-Pro.local>
2018-10-15 20:08:54 +0200

    added debug data , function, try symmetry breaking

--- HandWrite_Recognition/Learn.m
+++ HandWrite_Recognition/Learn.m
@@ -1,5 +1,7 @@
 %load data
-load('mnist.mat');
+%*load('mnist.mat');
+load('ex4data1.mat');
+m = size(X, 1);
 
 % X size: 60,000 *784
 X = double(trainX);
@@ -40,25 +42,30 @@ Display_Data(X(sel, :));
 Init_Theta = Initialize_Theta(Input_Neurons,Hiddden_Neurons,Output_Neurons);
 
 %Cost function regularized & grandient regularized
-lambda = 1;
-[J,Grad] = CostGradFunc(X_part,y_part,Init_Theta,Input_Neurons,Hiddden_Neurons,Output_Neurons,lambda);
+lambda = 0;
+%*[J,Grad] = costtest(X_part,y_part,Init_Theta,Input_Neurons,Hiddden_Neurons,Output_Neurons,lambda);
+[J,Grad] = nnCostFunction(Init_Theta, ...
+                                   Input_Neurons, ...
+                                   Hiddden_Neurons, ...
+                                   10, ...
+                                   X, y, lambda);
 %gradient check
 GradientCheck(lambda);
 
 %%Train Neural Network
 fprintf('\nTraining Neural Network... \n')
 
-%*theta = TrainNeurals(X_part,y_part,lambda,Init_Theta,Input_Neurons,Hiddden_Neurons,Output_Neurons);
+theta = TrainNeurals(X_part,y_part,lambda,Init_Theta,Input_Neurons,Hiddden_Neurons,Output_Neurons);
 
 %resize Theta
 num_theta1 = Hiddden_Neurons * (Input_Neurons +1);
-%*Theta1 = reshape(theta(1:num_theta1),Hiddden_Neurons,Input_Neurons+1);
-%*Theta2 =  reshape(theta(num_theta1+1:end),Output_Neurons,Hiddden_Neurons+1);
+Theta1 = reshape(theta(1:num_theta1),Hiddden_Neurons,Input_Neurons+1);
+Theta2 =  reshape(theta(num_theta1+1:end),Output_Neurons,Hiddden_Neurons+1);
 
 %predict the handwrite recognition and give accuracy
-%*Prediction = predict(Theta1,Theta2,X_part);
-%*accuracy = mean(double(Prediction == y_part)) * 100;
-%*fprintf('\nLearning Accuracy: %f\n', accuracy)
+Prediction = predict(Theta1,Theta2,X_part);
+accuracy = mean(double(Prediction == y_part)) * 100;
+fprintf('\nLearning Accuracy: %f\n', accuracy)
 
 %Build Learning Curve to see next step
 [error_train, error_val] = LearningCurve(X_part(1:100,:),y_part(1:100,:),CV_X(1:100,:),CV_Y(1:100,:),lambda,Input_Neurons,Hiddden_Neurons,Output_Neurons,Init_Theta);
