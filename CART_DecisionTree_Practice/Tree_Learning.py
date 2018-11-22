dfcabdb7037de5f2a370d9a5e451b285e6ef9f2d
huize mao <2041125835@qq.com>
2018-11-22 18:15:51 +0100

    finished building trees using recursive method

--- CART_DecisionTree_Practice/Tree_Learning.py
+++ CART_DecisionTree_Practice/Tree_Learning.py
@@ -29,12 +29,53 @@ def get_split(dataset):
         for row in dataset:
             groups = draft_split(n,row[n],dataset)
             gini = round(GiniCostFunc(groups,y),3)
-            print('')
             print("x{} < {} Gini Score = {} ".format(n+1,row[n],gini))
-            if gini < ThresholdVal:
-                n_ind,ThresholdVal,gini_score, split_groups = n, row[n],gini,groups
+            if gini < gini_score:
+                n_ind,ThresholdVal,gini_score, split_groups = n,row[n],gini,groups
     return {'feature_index':n_ind,'ThresholdVal':ThresholdVal,'groups':split_groups}
-    split,gini_score,
+    split,gini_score
+
+def get_terminal(group):
+    y = [row[-1] for row in group]
+    return max(set(y),key = y.count)
+
+def split(node,max_depth,min_size,depth): #node is a dictionary contains group split info
+    left,right = node['groups']
+    del node['groups']
+    #check if the next split only have one child
+    if not left or not right:
+        node['left'] = node['right'] = get_terminal(left + right)
+        return
+    #check if the recursion reach the max number 
+    if depth >= max_depth:
+        node['left'],node['right'] = get_terminal(left),get_terminal(right)
+        return
+    #process left child 
+    if len(left) <= min_size:
+        node['left'] = get_terminal(left)
+    else:
+        node['left'] = get_split(left)
+        split(node['left'],max_depth,min_size,depth+1)
+    #process right child 
+    if len(right) <= min_size:
+        node['right'] = get_terminal(right)
+    else:
+        node['right'] = get_split(right)
+        split(node['right'],max_depth,min_size,depth+1)
+    
+def build_tree(dataset,max_depth,min_size):
+    root = get_split(dataset)
+    split(root,max_depth,min_size,1)
+    print(root)
+    return root  #root is a dictionary which has been modified during spliting 
+def print_tree(node, depth=0):
+	if isinstance(node, dict):
+		print('%s[X%d < %.3f]' % ((depth*' ', (node['feature_index']+1), node['ThresholdVal'])))
+		print_tree(node['left'], depth+1)
+		print_tree(node['right'], depth+1)
+	else:
+		print('%s[%s]' % ((depth*' ', node)))
+
 dataset = [[2.771244718,1.784783929,0],
 	[1.728571309,1.169761413,0],
 	[3.678319846,2.81281357,0],
@@ -46,4 +87,7 @@ dataset = [[2.771244718,1.784783929,0],
 	[10.12493903,3.234550982,1],
 	[6.642287351,3.319983761,1]]
 
-split = get_split(dataset)
+tree = build_tree(dataset,3,1)
+print_tree(tree)
+
+
